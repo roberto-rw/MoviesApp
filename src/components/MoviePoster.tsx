4a8e76bc578010d15af314c9812a0cdc708e2faf
Roberto RW <robertorw7@gmail.com>
2023-12-27 22:54:16 -0800

    feat: carousel

--- src/components/MoviePoster.tsx
+++ src/components/MoviePoster.tsx
@@ -1,29 +1,29 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { Movie } from "../interfaces/movieInterfaces";
 
+interface Props{
+    movie: Movie;
+}
 
-export default function MoviePoster() {
+export default function MoviePoster({movie}: Props) {
+    const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
     return (
-        <View>
-            <Text>MoviePoster</Text>
+        <View style={styles.view}>
+            <Image
+                source={{uri}}
+                style={styles.image}
+            />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    imageContainer: {
-        flex: 1,
-        borderRadius: 18,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 10
-        },
-        shadowOpacity: 0.24,
-        shadowRadius: 7,
-        elevation: 10
+    image:{
+        flex:1,
+        borderRadius:18
     },
-    image: {
-        flex: 1,
-        borderRadius: 18
+    view:{
+        width:300,
+        height:420,
     }
 });
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,17 @@
-import { ActivityIndicator, Text, View } from "react-native";
+import { ActivityIndicator, Dimensions, Text, View } from "react-native";
 import useMovies from "../hooks/useMovies";
+import MoviePoster from "../components/MoviePoster";
+import Carousel from 'react-native-snap-carousel';
+
+
+const windowWidth = Dimensions.get('window').width;
 
 export default function HomeScreen() {
     const {movies, isLoading} = useMovies();
 
     if(isLoading){
         return (
-            <View style={{flex:1}}>
+            <View style={{flex:1, justifyContent:'center', alignContent:'center'}}>
                 <ActivityIndicator color="red" size={100}/>
             </View>
         )
@@ -14,7 +19,12 @@ export default function HomeScreen() {
 
     return (
         <View>
-            <Text>HomeScreen</Text>
+            <Carousel
+                data={movies}
+                renderItem={({item}: any) => <MoviePoster movie={item}/>}
+                sliderWidth={windowWidth}
+                itemWidth={300}
+            />
         </View>
     );
 }
